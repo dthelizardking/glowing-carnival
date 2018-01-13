e8311a084dd0d35046d502095505eb486cf3f156
Derek  Krause <dthelizardking@gmail.com>
2018-01-12 22:01:44 -0800

    Day 3

---
+++ Day003/ex08/ft_atoi.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dkrause- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/12 16:39:36 by dkrause-          #+#    #+#             */
+/*   Updated: 2018/01/12 16:39:44 by dkrause-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	negativ;
+	int	number;
+
+	i = 0;
+	negativ = 0;
+	number = 0;
+	while ((str[i] == ' ') || (str[i] == '\t') || (str[i] == '\n')
+		|| (str[i] == '\v') || (str[i] == '\f') || (str[i] == '\r'))
+		i++;
+	if (str[i] == 45)
+		negativ = 1;
+	if ((str[i] == 45) || (str[i] == 43))
+		i++;
+	while (str[i] >= 48 && str[i] <= 57)
+	{
+		number *= 10;
+		number += ((int)str[i] - 48);
+		i++;
+	}
+	if (negativ == 1)
+		return (-number);
+	else
+		return (number);
+}
